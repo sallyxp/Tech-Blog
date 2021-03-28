985bc79f52007def5719c397814e18de4ba32608
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-03-28 15:22:00 +0100

    started seed index.js and user-seed.js

---
+++ seeds/index.js
@@ -0,0 +1,17 @@
+// this index is for the seeds
+const sequelize = require('../config/connection');
+const User = require('../models/User');
+const userData = require('./user-seeds.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+  
+    await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
+    });
+  
+    process.exit(0);
+  };
+  
+  seedDatabase();

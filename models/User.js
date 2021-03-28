9d21618473e865a238af00babfe6cc0951b95f94
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-03-28 15:16:48 +0100

    made User.js model

--- models/User.js
+++ models/User.js
@@ -1,7 +1,43 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+class User extends Model { }
 
-Post.init(
-    
+User.init(
+    {
+        id: { //user id
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: { //username
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: { //email, & validation is an  "email@email.com"
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: { //password - XXXXX will need to do more with this
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [5]
+            }
+        }
+},
+    {   
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+    }
+);
+
+module.exports = User;

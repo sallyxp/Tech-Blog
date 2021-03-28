c2866b970829f12a4ad93eb8c8df4e617e0e4e98
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-03-28 23:43:44 +0100

    locations tidied and post-seeds updated

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,6 @@
-const seedPosts = require('./post-seeds');
-const seedUsers = require('./user-seeds');
-const seedComments = require('./comment-seeds');
+const seedPosts = require('.seeds/post-seeds');
+const seedUsers = require('.seeds/user-seeds');
+const seedComments = require('.seeds/comment-seeds');
 
 const sequelize = require('../config/connection');
 
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -4,27 +4,27 @@ const postData = [
     {//1
         title: "Perpetual Motion Machine runs out of steam",
         post_content: "We obey the Laws of Physics in this Household says Homer Simpson",
-        user_id = 2
+        user_id: 2
     },
     {//2
         title: "if it's green and wiggles ....",
         post_content: "Got no nose? How doit's Biology.  If it stinks, it's Chemistry.",
-        user_id = 2
+        user_id: 2
     },
     {//3
         title: "Why does a burger have less energy than a steak?",
         post_content: "Because it's in its ground state",
-        user_id = 3
+        user_id: 3
     },
     {//3
         title: "Why can't you trust an atom?",
         post_content: "They make up everything",
-        user_id = 4
+        user_id: 4
     },
       {//4
         title: "A neutron walks into a bar ..",
         post_content: "and asks 'how much?",
-        user_id = 1
+        user_id: 1
     }
 
 

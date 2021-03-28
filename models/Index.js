2c33eed58ac4712fc1f00c29815fe6b5e9d30430
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-03-28 14:23:19 +0100

    added associations Index.js

---
+++ models/Index.js
@@ -0,0 +1,34 @@
+const Post = require('./Post');
+
+//create associations
+//A user has many posts, a post can have many comments, but a post can only have one user.
+//so 1:m user to post, 1:m post to comments
+
+User.hasMany(Post, { //one to many 
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, { //one to one
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {//one to many 
+    foreignKey: 'post_id'
+});
+
+Comment.belongsTo(Post, { //one to one 
+    foreignKey: 'post_id'
+});
+
+//don't believe this is necessary.
+//Comment.belongsTo(User, { //many to one
+//    foreignKey: 'user_id'
+//});
+
+//User.hasMany(Comment, { //one to many 
+//    foreignKey: 'user_id'
+//});
+  
+
+
+module.exports = {Comment, Post, User};

b94cd5622dda8f86fcc9ea5f06f7c18d020c05af
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-03-29 01:10:23 +0100

    updated server.js with more links and improved seeds/index.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const exphbs = require('express-handlebars');
+
 
 
 const app = express();
@@ -8,11 +10,14 @@ const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 
+const hbs = exphbs.create({});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(require('./controllers'));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));

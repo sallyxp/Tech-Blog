0de51032ae4a24eb7e2a96d22c4847a83f0b0c8d
Sally Rodgers <sallyxp@yahoo.co.uk>
2021-03-30 22:16:12 +0100

    main.handlebars & asstd form structure in style of 10&BookInv, dashboard-routes added too.

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
+//create connection to database, pass in personal MySQL info
+//changed for Heroku
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3306,
-  }
-);
+    port: 3306
+  });
+}
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,23 +1,30 @@
-const path = require('path');
 const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+
+
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 
-
+//const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
-const sequelize = require('./config/connection');
-
-const hbs = exphbs.create({});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use(require('./controllers'));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
